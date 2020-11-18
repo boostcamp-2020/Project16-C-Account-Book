452cd4c34570db5818a3794160972e93de9c85e4
msmk530 <msmk530@naver.com>
2020-11-18 18:34:30 +0900

    [chore] #1 프론트 개발환경 설정
    - webpack, babel, prettier, eslint 설정
    
    - tsx파일에서 프리티어 적용실패

---
+++ FE/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.d.ts', '.ts', '.tsx'],
+      },
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { js: 'never', jsx: 'never', ts: 'never', tsx: 'never', json: 'never' },
+    ],
+    'react/jsx-indent': [2, 'tab'],
+    'react/jsx-indent-props': [2, 'tab'],
+  },
+};
---
+++ FE/src/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+console.log("hi");
+const App = props => {
+  return <h1>hello</h1>;
+};
+
+export default App;
