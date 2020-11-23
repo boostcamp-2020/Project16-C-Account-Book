5f2ca9500e711d612e521b4ddcfa5fbc5f749fa6
hi0826 <kkkttt555@naver.com>
2020-11-23 14:46:42 +0900

    [feat] #3 라우터 연결 변경
    - 라우터 연결방식 변경

--- BE/src/loaders/koa.ts
+++ BE/src/loaders/koa.ts
@@ -2,15 +2,14 @@ import cors from '@koa/cors';
 import bodyParser from 'koa-bodyparser';
 import Router from 'koa-router';
 
-import home from '../routes/index';
+import indexRouter from '../routes/index';
 
 const router = new Router();
 
 export default async (app: any): Promise<void> => {
   app.use(cors());
   app.use(bodyParser());
+  app.use(router.routes()).use(router.allowedMethods());
 
-  router.use('/', home.routes());
-  app.use(router.routes());
-  app.use(router.allowedMethods());
+  router.use(indexRouter.routes());
 };
