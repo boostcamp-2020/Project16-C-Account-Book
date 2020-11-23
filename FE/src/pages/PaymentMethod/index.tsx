56ad9728e121d9a0d8a63b533619c9bfc698cf01
msmk530 <msmk530@naver.com>
2020-11-23 11:33:36 +0900

    [feat] page에 payment method 컴포넌트들 조합 - modal식으로 보여주기 위해 modal state로 조작

---
+++ FE/src/pages/PaymentMethod/index.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+import Modal from '../../components/PaymentMethod/Modal';
+import MenuBar from '../../components/Common/MenuBar';
+import styles from './paymentPage.module.scss';
+
+export default function PaymentMethod() {
+  const [modal, setModal] = useState(false);
+
+  return (
+    <div className={styles.wrapper}>
+      <MenuBar setModal={setModal} />
+      {modal && <Modal setModal={setModal} />}
+    </div>
+  );
+}
