2183d97c500b3e72118c0b3a8ca1ac1b2d3b4e69
hi0826 <kkkttt555@naver.com>
2020-11-23 21:36:30 +0900

    [feat] #14 user controller 구현
    - 다같이 API 가닥잡기
    - user controller
      - get
      - post

--- BE/src/controllers/user.ts
+++ BE/src/controllers/user.ts
@@ -1,8 +1,16 @@
 import { Context } from 'koa';
+import service from '@/services/user';
 
-const get = (ctx: Context): Context['body'] => {
-  ctx.body = `GET ${ctx.path}`;
+const get = async (ctx: Context): Promise<Context['body']> => {
+  const user = await service.get('asdfasdfasdf');
+  ctx.body = user;
   return ctx.body;
 };
 
-export default { get };
+const post = async (ctx: Context): Promise<Context['body']> => {
+  const userId = await service.post(ctx.request.body);
+  ctx.body = userId;
+  return ctx.body;
+};
+
+export default { get, post };
