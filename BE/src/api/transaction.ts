14af00d097b9819f208136da09e687cf3c63e101
hi0826 <kkkttt555@naver.com>
2020-11-23 15:25:36 +0900

    [refactor] #3 BE 환경설정 코드 합치기
    - 백엔드 환경설정 코드 합치기

--- BE/src/api/transaction.ts
+++
@@ -1,9 +0,0 @@
-import Router from 'koa-router';
-
-const transactionRouter = new Router();
-
-transactionRouter.get('/', (ctx, next) => {
-  ctx.body = 'transaction router';
-});
-
-export default transactionRouter;
--- BE/src/api/user.ts
+++
@@ -1,9 +0,0 @@
-import Router from 'koa-router';
-
-const userRouter = new Router();
-
-userRouter.get('/', (ctx, next) => {
-  ctx.body = 'user router';
-});
-
-export default userRouter;
