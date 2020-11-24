c8c025ecff59cbfeeb4a9ad13bb045029557310f
msmk530 <msmk530@naver.com>
2020-11-24 11:10:50 +0900

    [feat] #10 임시 취소버튼

--- FE/src/components/PaymentMethod/CardContainer/index.tsx
+++ FE/src/components/PaymentMethod/CardContainer/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+
 import { paymentContext } from '../../../store/PaymentMethod/paymentMethodContext';
 import { useRootData } from '../../../store/PaymentMethod/paymentMethodHook';
 import styles from './cardContainer.module.scss';
@@ -12,6 +13,7 @@ export default function CardContainer(): React.ReactElement {
     <div className={styles.container} data-overlay>
       {paymentMethods.map(card => (
         <div className={styles.card} style={{ background: `${card.color}` }}>
+          <div className={styles.cancel}>X</div>
           <div className={styles.cardTitle}>{card.name}</div>
           <div className={styles.cardDesc}>{card.desc}</div>
         </div>
