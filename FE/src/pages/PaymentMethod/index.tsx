5797ad6fa21d277b691492c445be3d7d66df4e78
msmk530 <msmk530@naver.com>
2020-11-24 19:30:23 +0900

    [feat] #10 Database로부터 Default 결제수단을 받아와서 사용 할 수 있도록 연결
    - default 선택지를 받아오는 것이기 때문에 한번 셋팅되면 서비스 내내 변화되지 않을 것이므로 의존성 배열에넣지 않았습니다.
    
    - getData 함수는 한번 정의되면 새로 정의할 필요가 없기 때문에 useCallback을 통해 정의하였습니다.

--- FE/src/pages/PaymentMethod/index.tsx
+++ FE/src/pages/PaymentMethod/index.tsx
@@ -1,24 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import Modal from '../../components/PaymentMethod/Modal';
 import MenuBar from '../../components/Common/MenuBar';
 import styles from './paymentPage.module.scss';
+import { getDefaultMethods } from '../../api/defaultPaymentMethod';
 
 export default function PaymentMethod() {
   const [modal, setModal] = useState(true);
+  const [defaultMethod, setDefaultMethod] = useState([]);
 
-  // 백엔드에서 받아올 default 결제수단.
-  const defaultMethod = [
-    { name: 'KB 국민카드', color: 'hsla(0, 100%, 50%, 0.93)' },
-    { name: 'Kakao ', color: 'hsla(40, 100%, 50%, 0.93)' },
-    { name: 'SC 제일은행', color: 'hsla(80, 100%, 50%, 0.93)' },
-    { name: 'Naver ', color: 'hsla(120, 100%, 50%, 0.93)' },
-    { name: 'KEB Hana ', color: 'hsla(160, 100%, 50%, 0.93)' },
-    { name: 'WOORI Card', color: 'hsla(200, 100%, 50%, 0.93)' },
-    { name: 'Samsung', color: 'hsla(240, 100%, 50%, 0.93)' },
-    { name: 'Hyundai', color: 'hsla(280, 100%, 50%, 0.93)' },
-    { name: 'BC Card', color: 'hsla(320, 100%, 50%, 0.93)' },
-  ];
+  const getData = useCallback(async () => {
+    const datas = await getDefaultMethods();
+    setDefaultMethod(datas);
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div className={styles.wrapper}>
