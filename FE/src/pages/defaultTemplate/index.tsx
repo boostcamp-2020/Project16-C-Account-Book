f22137f8b4107460948858f70e6996ed7d03bbbd
msmk530 <msmk530@naver.com>
2020-11-25 21:27:16 +0900

    [refactor] 기존에 사용하던 페이지를 앞으로 만들 페이지들의 템플릿 느낌으로 수정
    - header포함 및 결제수단 모달 set하는 함수 등

---
+++ FE/src/pages/defaultTemplate/index.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import Modal from '../../components/PaymentMethod/Modal';
+import MenuBar from '../../components/Common/MenuBar';
+
+import styles from './paymentPage.module.scss';
+import { getDefaultMethods } from '../../api/defaultPaymentMethod';
+
+export default function PaymentMethod() {
+  const [modal, setModal] = useState(false);
+  const [defaultMethod, setDefaultMethod] = useState([]);
+
+  const getData = async () => {
+    const datas = await getDefaultMethods();
+    setDefaultMethod(datas);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className={styles.wrapper}>
+      <MenuBar setModal={setModal} />
+
+      {modal && <Modal setModal={setModal} defaultMethod={defaultMethod} />}
+    </div>
+  );
+}
