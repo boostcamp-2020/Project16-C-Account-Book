b072dc1e28679154ac8a81b71776363d0d6e7480
msmk530 <msmk530@naver.com>
2020-11-25 21:22:17 +0900

    [feat] 라우팅에 따른 구조 변경 및 날짜 계산을 위한 로직 분리

---
+++ FE/src/util/calculateDate.ts
@@ -0,0 +1,57 @@
+const CalculateDate = {
+  monList: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+  dayList: [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ],
+  today: new Date(),
+  monForChange: new Date().getMonth(),
+  activeDate: new Date(),
+
+  getFirstDay: (yearInfo, monthInfo) => new Date(yearInfo, monthInfo, 1),
+  getLastDay: (yearInfo, monthInfo) => new Date(yearInfo, monthInfo + 1, 0),
+
+  nextMonth() {
+    const d = new Date();
+    d.setDate(1);
+    d.setMonth(++this.monForChange);
+    this.activeDate = d;
+    return d;
+  },
+  prevMonth() {
+    const d = new Date();
+    d.setDate(1);
+    d.setMonth(--this.monForChange);
+    this.activeDate = d;
+    return d;
+  },
+  addZero: num => (num < 10 ? `0${num}` : num),
+  activeDTag: null,
+  getIndex(node) {
+    let index = 0;
+    while ((node = node.previousElementSibling)) {
+      index++;
+    }
+    return index;
+  },
+};
+
+export default CalculateDate;
