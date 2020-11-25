cbc277096adca8d62ef37a5c281d65645315374a
msmk530 <msmk530@naver.com>
2020-11-25 21:16:59 +0900

    [feat] 만든 페이지(DefaultTemplate, Calendar)라우팅

--- FE/src/app.tsx
+++ FE/src/app.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import PaymentProvider from './store/PaymentMethod/paymentMethodContext';
+import DateInfoProvider from './store/DateInfo/dateInfoContext';
 import './app.scss';
 
-import PaymentMethod from './pages/PaymentMethod';
+import PaymentMethod from './pages/defaultTemplate';
+import Calendar from './pages/Calendar';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = props => {
   return (
-    <PaymentProvider>
-      <PaymentMethod />
-    </PaymentProvider>
+    <DateInfoProvider>
+      <PaymentProvider>
+        <BrowserRouter>
+          <Route path="/" component={PaymentMethod} />
+          <Route path="/calendar" component={Calendar} />
+        </BrowserRouter>
+      </PaymentProvider>
+    </DateInfoProvider>
   );
 };
 
