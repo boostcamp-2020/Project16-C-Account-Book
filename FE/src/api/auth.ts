9b845921ba049fc801b6ba8b79d042445f8bca77
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-11-25 18:51:26 +0900

    [feat] #6 github에서 callback을 받아서 백엔드에 요청하는 컴포넌트 생성
    - 깃헙에 사용자가 로그인을 한 후에 callback을 받아서 서버에 토큰을 달라고 요청을 보내는 과정을 담당하는 컴포넌트 생성

--- FE/src/api/auth.ts
+++ FE/src/api/auth.ts
@@ -4,11 +4,12 @@ import jwtDecode from 'jwt-decode';
 import { postFetch } from '../service/fetch';
 
 const login = async (code: string) => {
-  const tokenGetUrl = 'http://localhost:3000/api/auth/login';
+  const backendPath = process.env.BACKEND;
+  const tokenGetUrl = `${backendPath}/api/auth/login`;
   try {
     const { token } = await postFetch(tokenGetUrl, { code });
     window.localStorage.setItem('token', token);
-    console.log(jwtDecode(token));
+    return token;
   } catch (error) {
     console.error(error);
   }
