94bb264f93b7a90f4fd4447c9a9192c0ee4cf224
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-11-26 17:52:34 +0900

    [feat] #29 auth 관련 api를 제외하고는 인증을 통과해야지 넘어가도록 미들웨어 추가
    - auth 관련 api는 인증을 통과하지 않아도 되어야한다. 예를들어, 로그인은 토큰을 받기 전이기 때문에 인증을 통과할 필요가 없다. 그 이외의 것들은 인증을 통과해야하므로 그 사이에 인증을 하는 미들웨어를 두었다.

--- BE/src/routes/api/index.ts
+++ BE/src/routes/api/index.ts
@@ -1,19 +1,24 @@
 import { Context } from 'koa';
 import Router from 'koa-router';
+
+import authController from '@/controllers/auth';
 import userRouter from './user';
 import authRouter from './auth';
 import accountBookRouter from './account-book';
 import defaultPaymentMethodRouter from './defaultPaymentMethod';
 
-
 const router = new Router();
 
-router.get('/', (ctx: Context) => {
+router.get('/', (ctx: Context, next) => {
   ctx.body = `GET ${ctx.path}`;
+  next();
 });
 
-router.use('/user', userRouter.routes());
 router.use('/auth', authRouter.routes());
+
+router.use(authController.checkToken);
+
+router.use('/user', userRouter.routes());
 router.use('/accountbook', accountBookRouter.routes());
 router.use('/defaultPaymentMethod', defaultPaymentMethodRouter.routes());
 
