1253d0843e01c29f196197a74a5419ab57d5794e
hi0826 <kkkttt555@naver.com>
2020-11-26 10:02:11 +0900

    [feat] #27 가계부 내역을 보여주는 컴포넌트
    - 가계부 내역에서 일자별로 계산
      - Set사용해서 데이터에서 일을 뽑아냄
      - Array로 만들고 reverse를 통해 역으로 순서를 바꿈
      - map 함수를 사용하여 일자에 맞게 뽑아서 해당하는 일에 붙여준다.

---
+++ FE/src/transaction/components/list/transactions/transactions.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+import styles from './transactions.module.scss';
+
+const makeContent = (category: string, desc: string, payment: string, cost: number) => {
+  return (
+    <div className={styles.transaction}>
+      <span className={styles.category}>{category}</span>
+      <span className={styles.description}>{desc}</span>
+      <span className={styles.payment}>{payment}</span>
+      <span className={styles.cost}>{cost}</span>
+    </div>
+  );
+};
+
+const makeTransactions = () => {
+  // axios로 비동기 처리
+  const transactions = [
+    { day: 1, category: '뷰티', description: '미용실', payment: '신한카드', cost: 14000 },
+    { day: 1, category: '뷰티', description: '미용실', payment: '신한카드', cost: 1 },
+    { day: 1, category: '뷰티', description: '미용실', payment: '신한카드', cost: 2 },
+    { day: 1, category: '뷰티', description: '미용실', payment: '신한카드', cost: 3 },
+    { day: 1, category: '뷰티', description: '미용실', payment: '신한카드', cost: 4 },
+    { day: 2, category: '밥', description: '김선생', payment: '신한카드', cost: 5 },
+    { day: 3, category: '쇼핑', description: '무신사', payment: '신한카드', cost: 163 },
+    { day: 3, category: '쇼핑', description: '나이키', payment: '신한카드', cost: 421 },
+    { day: 4, category: '저금', description: '카뱅', payment: '신한카드', cost: 123 },
+    { day: 4, category: '저금', description: '주택청약', payment: '신한카드', cost: 231 },
+    { day: 15, category: '쇼핑', description: '나이키', payment: '신한카드', cost: 22 },
+  ];
+
+  const days = transactions.map(value => value.day);
+  const daysArr = Array.from(new Set<number>(days)).reverse();
+
+  return (
+    <>
+      {daysArr.map(day => {
+        return (
+          <div className={styles.day}>
+            <div className={styles.daybar}> {day} 일</div>
+            {transactions
+              .filter(t => t.day === day)
+              .map(t => makeContent(t.category, t.description, t.payment, t.cost))}
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+const Transactions = () => {
+  return <>{makeTransactions()}</>;
+};
+
+export default Transactions;
