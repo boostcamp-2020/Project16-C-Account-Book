7a33ad0b71e6407bef372488bd349a6d785e1ec4
hi0826 <kkkttt555@naver.com>
2020-11-26 15:35:24 +0900

    [refactor] 폴더구조 맞게 변경, transaction 라우팅 추가
    - 폴더구조 맞게 변경
    - transaction 라우팅 추가

--- FE/src/app.tsx
+++ FE/src/app.tsx
@@ -8,6 +8,7 @@ import './app.scss';
 import DefaultTemplate from './pages/DefaultTemplate';
 import CalendarPage from './pages/Calendar';
 import LoginPage from './pages/Login';
+import TransactionPage from './pages/transaction';
 import GithubLoginProcess from './pages/Login/GithubLoginProcess';
 
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
             <Route exact path="/login" component={LoginPage} />
             <Route exact path="/auth/github" component={GithubLoginProcess} />
             <Route exact path="/calendar" component={CalendarPage} />
+            <Route exact path="/transaction" component={TransactionPage} />
           </Switch>
         </Router>
       </PaymentProvider>
--- FE/src/transaction/components/menubar/menubar.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './menubar.module.scss';
-
-const Menubar = props => {
-  return <div>메뉴바</div>;
-};
-
-export default Menubar;
