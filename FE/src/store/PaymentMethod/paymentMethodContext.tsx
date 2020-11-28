60f168f5c1c9fdacec748a71e90c2f7a174419c8
msmk530 <msmk530@naver.com>
2020-11-28 19:05:10 +0900

    [refactor] mobx 버전 향상에 따른 의존성 변경

--- FE/src/store/PaymentMethod/paymentMethodContext.tsx
+++ FE/src/store/PaymentMethod/paymentMethodContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react';
-import { useLocalStore } from 'mobx-react-lite';
+import { useLocalStore } from 'mobx-react';
 import { createStore, TStore } from './paymentMethodStore';
 
 export const paymentContext = createContext<TStore | null>(null);
