713e973ce852549ec1fdda03721fc7eed1fbd4d8
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-11-30 20:08:51 +0900

    [feat] user model social 추가
    - social 값도 추가해야 어느 소셜 로그인으로 로그인 했는지 알 수 있기 때문에 모델에 social 값도 추가했습니다.

--- BE/src/models/user.ts
+++ BE/src/models/user.ts
@@ -4,24 +4,26 @@ import { iUser } from '@/types/auth';
 export interface iUserDoc extends mongoose.Document {
   id: string;
   name: string;
+  social: string;
 }
 
 const userSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
+  id: { type: String, required: true },
   name: { type: String },
+  social: { type: String, required: true },
 });
 
-userSchema.index({ id: 1 });
+userSchema.index({ id: 1, social: 2 });
 
 const UserModel = mongoose.model<iUserDoc>('users', userSchema);
 
-const get = async ({ id }: iUser): Promise<iUserDoc | null> => {
-  const user = await UserModel.findOne({ id });
+const get = async ({ id, social }: iUser): Promise<iUserDoc | null> => {
+  const user = await UserModel.findOne({ id, social });
   return user;
 };
 
-const create = async ({ id, name }: iUser): Promise<string> => {
-  const userData = new UserModel({ id, name });
+const create = async ({ id, name, social }: iUser): Promise<string> => {
+  const userData = new UserModel({ id, name, social });
   const user = await userData.save();
 
   console.log(`create user ${id}`);
