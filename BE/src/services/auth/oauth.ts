3ab478425d7dc18db3dea7a66959d6bb71acc2df
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-11-30 15:32:02 +0900

    [feat] #6 네이버 로그인 api 추가
    - 여러 소셜 로그인이 생겼을 때 최대한 같은 함수를 쓰고 변경을 덜 해도 되도록 함수를 변경했습니다.
    
    - 그런데 각 소셜 로그인에서 응답의 방식 같은 것이 같지 않아서 완전히 추상화를 시키지 못했습니다.

---
+++ BE/src/services/auth/oauth.ts
@@ -0,0 +1,88 @@
+import createError from 'http-errors';
+import axios from 'axios';
+
+import { iTokenGetParams, iUser } from '@/types/auth';
+
+const tokenGetParams: { [key: string]: iTokenGetParams } = {
+  github: {
+    client_id: process.env.GITHUB_CLIENT_ID || '',
+    client_secret: process.env.GITHUB_CLIENT_SECRET || '',
+  },
+  naver: {
+    client_id: process.env.NAVER_CLIENT_ID || '',
+    client_secret: process.env.NAVER_CLIENT_SECRET || '',
+    grant_type: 'authorization_code',
+  },
+};
+
+const tokenGetUrl: { [key: string]: string } = {
+  github: 'https://github.com/login/oauth/access_token',
+  naver: 'https://nid.naver.com/oauth2.0/token',
+};
+
+const accessTokenReg: { [key: string]: RegExp } = {
+  github: /access_token=(\w+)&/,
+  naver: /access_token=(\w+)&/,
+};
+
+const userInfoGetUrl: { [key: string]: string } = {
+  github: 'https://api.github.com/user',
+  naver: 'https://openapi.naver.com/v1/nid/me',
+};
+
+const getTokenFromRes = (data: any, social: string): string => {
+  try {
+    if (social === 'github') {
+      const regArr = data.match(accessTokenReg[social]);
+      return regArr[1];
+    }
+    if (social === 'naver') {
+      return data.access_token;
+    }
+    throw createError(401, `${social} login is not supported`);
+  } catch (error) {
+    throw createError(401, 'code expired');
+  }
+};
+
+export const getAccessToken = async (code: string, social: string) => {
+  const { data } = await axios.post(tokenGetUrl[social], null, {
+    params: {
+      code,
+      ...tokenGetParams[social],
+    },
+  });
+
+  const token = getTokenFromRes(data, social);
+
+  return token;
+};
+
+export const getUserInfo = async (
+  accessToken: string,
+  social: string,
+): Promise<iUser> => {
+  const { data } = await axios.get(userInfoGetUrl[social], {
+    headers: {
+      Authorization: `BEARER ${accessToken}`,
+    },
+  });
+
+  console.log('data: ', data);
+
+  if (social === 'github') {
+    const userInfo = { id: data.login, name: data.name, social };
+    return userInfo;
+  }
+  if (social === 'naver') {
+    const userInfo = {
+      id: data.response.email,
+      name: data.response.name,
+      social,
+    };
+    console.log(userInfo);
+
+    return userInfo;
+  }
+  throw createError(401, `${social} login is not supported`);
+};
--- BE/src/types/auth.ts
+++ BE/src/types/auth.ts
@@ -1,10 +1,18 @@
 export interface iTokenGetParams {
-  code: string;
+  code?: string;
   client_id: string;
   client_secret: string;
+  grant_type?: string;
+  state?: string;
 }
 
 export interface iUser {
   id: string;
+  social: string;
   name?: string;
+}
+
+export interface iOAuth {
+  github?: string;
+  naver?: string;
 }
