c48aa4308b6baf888087fadf5ec702744b2bbb23
hi0826 <kkkttt555@naver.com>
2020-12-01 18:28:10 +0900

    [feat] #8 user 스키마 변경
    - password 속성 필요없어 삭제

--- BE/src/models/user/schema.ts
+++ BE/src/models/user/schema.ts
@@ -3,13 +3,11 @@ import mongoose from 'mongoose';
 export interface User extends mongoose.Document {
   id: string;
   name: string;
-  password: string;
 }
 
-const Schema = new mongoose.Schema({
+export const Schema = new mongoose.Schema({
   id: { type: String, required: true },
   name: { type: String, required: true },
-  password: { type: String },
 });
 
 export const UserModel = mongoose.model<User>('users', Schema);
