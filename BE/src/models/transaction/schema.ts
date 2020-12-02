b91b9693a8ac80c6bf99c562994b53153639efcb
hi0826 <kkkttt555@naver.com>
2020-12-02 09:53:57 +0900

    [feat] #8 transaction 스키마 추가
    - 거래내역 스키마 및 모델 추가
    - 모델은 사용하지 않으므로 추후 제거 가능성 있음

--- BE/src/models/transaction/schema.ts
+++ BE/src/models/transaction/schema.ts
@@ -8,17 +8,22 @@ import {
 export interface Transaction extends mongoose.Document {
   content: string;
   type: string;
-  categories: { category: Category };
+  category: { category: Category };
   cost: number;
   date: Date;
-  payments: { payment: PaymentMethod };
+  payment: { payment: PaymentMethod };
 }
 
 export const Schema = new mongoose.Schema({
   content: { type: String, required: true },
   type: { type: String, required: true },
   cost: { type: Number, required: true },
-  categories: { type: CategorySchema, required: true },
+  category: { type: CategorySchema, required: true },
   date: { type: Date, required: true },
-  payments: { type: PaymentSchema, required: true },
+  payment: { type: PaymentSchema, required: true },
 });
+
+export const TransactionModel = mongoose.model<Transaction>(
+  'transactions',
+  Schema,
+);
