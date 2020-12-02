8b34958a824ace6c18ed45897a660fe1298c68e9
hi0826 <kkkttt555@naver.com>
2020-12-02 09:55:07 +0900

    [feat] #8 accountbook service 변경, 개발
    - accountbook 모델 함수 변경으로 코드수정
    - accountbook model import 시 좀 더 직관적으로 변수명 변경
    - 거래내역 추가시 기능개발할 수 있도록 service 추가

--- BE/src/services/account-book.ts
+++ BE/src/services/account-book.ts
@@ -1,14 +1,26 @@
-import accountBookMethodModel from '@models/accountbook';
+import accountBookModel from '@models/accountbook';
 import { Context } from 'koa';
 
 const get = async (): Promise<any> => {
-  const accountBookMethods = await accountBookMethodModel.get();
+  const accountBookModels = await accountBookModel.get();
 
-  return accountBookMethods;
+  return accountBookModels;
 };
 
 const post = async (body: Context['body']): Promise<any> => {
-  await accountBookMethodModel.create(body.name, body.description, body.userId);
+  await accountBookModel.create(body.name, body.description, body.userId);
 };
 
-export default { get, post };
+const addTransaction = async (body: Context['body']): Promise<any> => {
+  const transactionInfo = {
+    content: body.content,
+    type: body.type,
+    category: body.category,
+    cost: body.cost,
+    date: body.date,
+    payment: body.payment,
+  };
+  await accountBookModel.addTransaction(body.accountBookId, transactionInfo);
+};
+
+export default { get, post, addTransaction };
