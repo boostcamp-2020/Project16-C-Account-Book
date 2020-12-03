0e858c79e5422f210e4041798cb575b90fe99287
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-12-03 18:44:58 +0900

    [feat] 카테고리나 타입이 선택되었을 때 동작 추가
    - 상위 컴포넌트에서 보낸 props를 이용해서 선택되었을 때 동작을 추가했다.

--- FE/src/components/transaction/list/listcontainer.tsx
+++ FE/src/components/transaction/list/listcontainer.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './listcontainer.module.scss';
 
 import Filter from './filter/listfilter';
 import Transactions from './transactions/transactions';
 
-const ListContainer = props => {
+const ListContainer = () => {
+  const [selectedCategory, selectCategory] = useState('all');
+  const [selectedTypes, selectType] = useState(['수입', '지출']);
+
   return (
     <div className={styles.container}>
-      <Filter />
-      <Transactions />
+      <Filter
+        selectedCategory={selectedCategory}
+        selectCategory={selectCategory}
+        selectedTypes={selectedTypes}
+        selectType={selectType}
+      />
+      <Transactions
+        selectedCategory={selectedCategory}
+        selectedTypes={selectedTypes}
+      />
     </div>
   );
 };
