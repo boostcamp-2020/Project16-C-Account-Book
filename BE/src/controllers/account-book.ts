c2c42ec71098a5163b5025c631b19ecbf19853f6
hi0826 <kkkttt555@naver.com>
2020-12-03 14:18:55 +0900

    [feat] #39 #38 #20 #19 response 생성 함수 추가
    - response 객체 만들어주는 함수 util에 생성

--- BE/src/controllers/account-book.ts
+++ BE/src/controllers/account-book.ts
@@ -1,55 +1,39 @@
 import { Context } from 'koa';
 import service from '@services/account-book';
-import Response from '@interfaces/response';
+import { response } from '@utils/response';
 
 const get = async (ctx: Context): Promise<Context['body']> => {
   const accountBooks = await service.get(ctx);
-  const response: Response = {
-    status: 200,
-    message: 'success',
-    data: accountBooks,
-  };
-  ctx.status = response.status;
-  ctx.body = response;
+  const res = response(200, 'success', accountBooks);
+  ctx.status = res.status;
+  ctx.body = res;
 
   return ctx.body;
 };
 
 const post = async (ctx: Context): Promise<Context['body']> => {
   const accountBook = await service.post(ctx);
-  const response: Response = {
-    status: 200,
-    message: 'success',
-    data: accountBook,
-  };
-  ctx.status = response.status;
-  ctx.body = response;
+  const res = response(200, 'success', accountBook);
+  ctx.status = res.status;
+  ctx.body = res;
 
   return ctx.body;
 };
 
 const update = async (ctx: Context): Promise<Context['body']> => {
   const updateResult = await service.patch(ctx);
-  const response: Response = {
-    status: 200,
-    message: 'success',
-    data: updateResult,
-  };
-  ctx.status = response.status;
-  ctx.body = response;
+  const res = response(200, 'success', updateResult);
+  ctx.status = res.status;
+  ctx.body = res;
 
   return ctx.body;
 };
 
 const del = async (ctx: Context): Promise<Context['body']> => {
   const deleteResult = await service.del(ctx);
-  const response: Response = {
-    status: 200,
-    message: 'success',
-    data: deleteResult,
-  };
-  ctx.status = response.status;
-  ctx.body = response;
+  const res = response(200, 'success', deleteResult);
+  ctx.status = res.status;
+  ctx.body = res;
 
   return ctx.body;
 };
