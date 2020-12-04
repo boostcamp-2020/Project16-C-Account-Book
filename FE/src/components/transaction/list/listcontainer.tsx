a00ec206b6cfb2ea628221d98919ee29d9c2092a
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-12-04 23:34:18 +0900

    [feat] #44 가계부 내역 추가 모달창을 띄우는 버튼 작성
    -가계부 내역 추가 모달창을 띄우는 버튼 작성

--- FE/src/components/transaction/list/listcontainer.tsx
+++ FE/src/components/transaction/list/listcontainer.tsx
@@ -1,26 +1,45 @@
 import React, { useState } from 'react';
-import styles from './listcontainer.module.scss';
+import './listcontainer.scss';
+import { PlusCircleIcon } from '@primer/octicons-react';
 
 import Filter from './filter/listfilter';
-import Transactions from './transactions/transactions';
+import Transactions from './transactions';
 
-const ListContainer = () => {
+const ListContainer = ({
+  setTransactionAddModal,
+}: {
+  setTransactionAddModal: (value: boolean) => void;
+}) => {
   const [selectedCategory, selectCategory] = useState('all');
   const [selectedTypes, selectType] = useState(['수입', '지출']);
 
+  const onAddButtonClicked = () => {
+    setTransactionAddModal(true);
+  };
+
   return (
-    <div className={styles.container}>
-      <Filter
-        selectedCategory={selectedCategory}
-        selectCategory={selectCategory}
-        selectedTypes={selectedTypes}
-        selectType={selectType}
-      />
-      <Transactions
-        selectedCategory={selectedCategory}
-        selectedTypes={selectedTypes}
-      />
-    </div>
+    <>
+      <div className="transaction__list__container">
+        <Filter
+          selectedCategory={selectedCategory}
+          selectCategory={selectCategory}
+          selectedTypes={selectedTypes}
+          selectType={selectType}
+        />
+        <Transactions
+          selectedCategory={selectedCategory}
+          selectedTypes={selectedTypes}
+        />
+      </div>
+      <button
+        className="transaction__add__button"
+        type="button"
+        onClick={onAddButtonClicked}
+      >
+        <PlusCircleIcon className="icon" />
+        거래내역 추가
+      </button>
+    </>
   );
 };
 
