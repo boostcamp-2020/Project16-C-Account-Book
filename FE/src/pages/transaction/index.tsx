69b6c7a1f3c14dbfe0db63b618c47b6212cebac2
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-12-04 13:36:19 +0900

    [feat] #44 상태에 따라서 모달창 띄우는 것 구현
    - 상태에 따라서 TransactionAddModal을 띄웁니다.

--- FE/src/pages/transaction/index.tsx
+++ FE/src/pages/transaction/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Modal from '../../components/PaymentMethod/Modal';
 import MenuBar from '../../components/Common/MenuBar';
 import ListContainer from '../../components/transaction/list/listcontainer';
+import TransactionAddModal from '../../components/transaction/TransactionAddModal';
 import styles from './transaction.module.scss';
 
 import useDefaultPayment from '../../service/useDefaultPayment';
@@ -12,13 +13,18 @@ export default function TransactionComponent(props) {
   useLoginCheck();
   const [paymentMethodModal, setPaymentMethodModal] = useState(false);
   const defaultMethod = useDefaultPayment();
+  const [transactionAddModal, setTransactionAddModal] = useState(false);
 
   return (
     <div className={styles.container}>
       <MenuBar setModal={setPaymentMethodModal} pageType="transaction" />
-      <ListContainer />
+      <ListContainer setTransactionAddModal={setTransactionAddModal} />
       {paymentMethodModal && (
         <Modal setModal={setPaymentMethodModal} defaultMethod={defaultMethod} />
+      )}
+
+      {transactionAddModal && (
+        <TransactionAddModal setTransactionAddModal={setTransactionAddModal} />
       )}
     </div>
   );
