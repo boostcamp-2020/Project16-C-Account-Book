9ec9def8c415d3baa28c0603baa433cbd437b7e8
hi0826 <kkkttt555@naver.com>
2020-12-05 12:34:27 +0900

    [feat] #46 내역 controller
    - 내역 controller 기능추가
    - patch는 추후 더 구현

--- BE/src/controllers/transaction.ts
+++ BE/src/controllers/transaction.ts
@@ -1,11 +1,16 @@
 import { Context } from 'koa';
+import service from '@services/transaction';
+import { response } from '@utils/response';
 
-const post = (ctx: Context): Context['body'] => {
-  ctx.body = `POST ${ctx.url}`;
+const post = async (ctx: Context): Promise<Context['body']> => {
+  const transaction = await service.post(ctx);
+  const res = response(200, transaction.message, transaction.data);
+  ctx.body = res;
   return ctx.body;
 };
 
-const patch = (ctx: Context): Context['body'] => {
+const patch = async (ctx: Context): Promise<Context['body']> => {
+  const updateResult = await service.patch(ctx);
   ctx.body = `PATCH ${ctx.url}`;
   return ctx.body;
 };
