bc3472cf0da6dc81fafb23a4d9acec16bfdee994
hi0826 <kkkttt555@naver.com>
2020-12-05 12:33:49 +0900

    [feat] #46 내역추가 api 변경
    - 내역추가 api accountbook service에서 transaction service로 이동

---
+++ BE/src/services/transaction.ts
@@ -0,0 +1,47 @@
+import accountBookModel from '@models/accountbook';
+import { Context } from 'vm';
+
+const post = async (ctx: Context): Promise<any> => {
+  const transactionInfo = {
+    content: ctx.request.body.content,
+    type: ctx.request.body.type,
+    category: ctx.request.body.category,
+    cost: ctx.request.body.cost,
+    date: ctx.request.body.date,
+    payment: ctx.request.body.payment,
+  };
+  const transaction = await accountBookModel.addTransaction(
+    ctx.params.accountbookid,
+    transactionInfo,
+  );
+  if (transaction) {
+    return {
+      message: 'success',
+      data: transaction,
+    };
+  }
+  return {
+    message: 'fail',
+    data: {},
+  };
+};
+
+const patch = async (ctx: Context): Promise<any> => {
+  const accountBookId = ctx.params.accountbookid;
+  const transactionId = ctx.params.transactionid;
+  const updateInfo = {
+    content: ctx.request.body.content,
+    type: ctx.request.body.type,
+    category: ctx.request.body.category,
+    cost: ctx.request.body.cost,
+    date: ctx.request.body.date,
+    payment: ctx.request.body.payment,
+  };
+  const updateResult = await accountBookModel.updateTransaction(
+    accountBookId,
+    transactionId,
+    updateInfo,
+  );
+};
+
+export default { post, patch };
