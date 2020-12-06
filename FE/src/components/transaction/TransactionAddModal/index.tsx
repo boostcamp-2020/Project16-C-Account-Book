922305ae8c916bbbbf4c931755be1958016bd98e
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-12-06 21:35:16 +0900

    [feat] #44 가계부 내역 추가 기능 구현
    - 가계부 내역 추가 기능 구현
    - 디비 스키마가 변경되면 타입을 추가하고 카테고리를 선택한 타입에 따라서 나오게 해야함.

--- FE/src/components/transaction/TransactionAddModal/index.tsx
+++ FE/src/components/transaction/TransactionAddModal/index.tsx
@@ -1,30 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 
 import { useTransactionData } from '../../../store/AccountBook/accountBookInfoHook';
 import DateInput from './DateInput';
 import CategoryInput from './CategoryInput';
 import PaymentInput from './PaymentInput';
 import PriceInput from './PriceInput';
-import MemoInput from './MemoInput';
 import MessageInput from './MessageInput';
+import ContentInput from './ContentInput';
+import { createTransaction } from '../../../api/transaction';
+
 import './TransactionAddModal.scss';
 
 const TransactionAddModal = ({
   setTransactionAddModal,
+  accountbookId,
 }: {
   setTransactionAddModal: (value: boolean) => void;
+  accountbookId: string;
 }) => {
-  const { categoryPool, paymentPool } = useTransactionData(store => ({
-    categoryPool: store.accountBook.category,
-    paymentPool: store.accountBook.payment,
+  const {
+    categoryPool,
+    paymentPool,
+    accountbook,
+    addTransaction,
+  } = useTransactionData(store => ({
+    accountbook: store.accountBook,
+    categoryPool: store.accountBook.categories,
+    paymentPool: store.accountBook.payments,
+    addTransaction: store.addTransaction,
   }));
 
   const today = new Date();
   const [year, setYear] = useState(today.getFullYear());
   const [month, setMonth] = useState(today.getMonth());
   const [day, setDay] = useState(today.getDate());
-  const [category, setCategory] = useState('');
-  const [payment, setPayment] = useState('');
+  const [category, setCategory] = useState({});
+  const [payment, setPayment] = useState({});
+  const [price, setPrice] = useState(0);
+  const [content, setContent] = useState('');
+
+  const closeModal = () => {
+    setTransactionAddModal(false);
+  };
 
   const onOverLayClicked = (e: Event) => {
     if (e.target === e.currentTarget) setTransactionAddModal(false);
@@ -34,26 +51,90 @@ const TransactionAddModal = ({
     e: React.MouseEvent<HTMLInputElement, MouseEvent>,
   ) => {
     const targetElement = e.target as HTMLInputElement;
-    setCategory(targetElement.value);
+    const newCategory = {
+      name: targetElement.dataset.name,
+      icon: targetElement.dataset.icon,
+    };
+    setCategory(newCategory);
   };
 
-  useEffect(() => {
-    console.log('category pool: ', categoryPool);
-  });
+  const onPaymentChange = (
+    e: React.MouseEvent<HTMLInputElement, MouseEvent>,
+  ) => {
+    const targetElement = e.target as HTMLInputElement;
+    const newPayment = {
+      name: targetElement.dataset.name,
+      description: targetElement.dataset.description,
+    };
+    console.log(newPayment);
+
+    setPayment(newPayment);
+  };
+
+  const onPriceChange = (e: Event) => {
+    if (e.target) {
+      const targetElement = e.target as HTMLInputElement;
+      setPrice(+targetElement.value);
+    }
+  };
+
+  const onContentChange = (e: Event) => {
+    if (e.target) {
+      const targetElement = e.target as HTMLInputElement;
+      setContent(targetElement.value);
+    }
+  };
+
+  const onSubmitClicked = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const date = `${year}-${month + 1}-${day}`;
+    const newTransaction = {
+      date,
+      type: '지출',
+      category,
+      cost: price,
+      payment,
+      content,
+    };
+
+    try {
+      const { status, data } = await createTransaction(
+        accountbookId,
+        newTransaction,
+      );
+      if (status !== 200) throw new Error();
+      addTransaction(data);
+      closeModal();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="transaction__modal" onClick={onOverLayClicked}>
       <div className="form__container">
         <h2>거래내역 추가</h2>
-        <form action="">
+        <form onSubmit={onSubmitClicked}>
           <DateInput
             {...{ year, month, today, day, setYear, setMonth, setDay }}
           />
           <CategoryInput {...{ categoryPool, category, onCategoryChange }} />
-          <PaymentInput />
-          <PriceInput />
-          <MemoInput />
+          <PaymentInput {...{ paymentPool, onPaymentChange }} />
+          <PriceInput {...{ onPriceChange }} />
+          <ContentInput {...{ onContentChange }} />
           <MessageInput />
+
+          <div className="transaction__button__container">
+            <button type="submit" className="transaction__submit__button">
+              확인
+            </button>
+            <input
+              type="reset"
+              className="transaction__cancel__button"
+              onClick={closeModal}
+              value="취소"
+            />
+          </div>
         </form>
       </div>
     </div>
