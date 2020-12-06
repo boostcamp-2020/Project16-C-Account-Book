a517c3a45ce4b38342fd96d0e295991436c5a3f1
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-12-06 21:32:11 +0900

    [feat] #44 선택된 날짜에 따라서 상태 변경
    - 선택된 날짜에 따라서 상태를 변경하는 기능 추가

--- FE/src/components/transaction/TransactionAddModal/DateInput/index.tsx
+++ FE/src/components/transaction/TransactionAddModal/DateInput/index.tsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
 import calculateDate from '../../../../util/calculateDate';
 import './index.scss';
 
 const DateInput = ({ year, month, today, day, setYear, setMonth, setDay }) => {
+  const onYearChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (e.target) {
+      const targetElement = e.target as HTMLSelectElement;
+      setYear(targetElement.value);
+    }
+  };
+  const onMonthChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (e.target) {
+      const targetElement = e.target as HTMLSelectElement;
+      setMonth(targetElement.value);
+    }
+  };
+  const onDayChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (e.target) {
+      const targetElement = e.target as HTMLSelectElement;
+      setDay(targetElement.value);
+    }
+  };
+
   return (
     <div className="item date__input">
       <div className="indicator">날짜</div>
       <div className="row">
         <div className="row__item">
-          <select name="year" defaultValue={year}>
+          <select name="year" defaultValue={year} onChange={onYearChange}>
             {Array.from(
               { length: 61 },
               (_, i) => i + +(today.getFullYear() - 30),
             ).map(item => (
-              <option value={item} selected={year === item}>
+              <option value={item} selected={year === item} key={item}>
                 {item}
               </option>
             ))}
@@ -22,9 +41,13 @@ const DateInput = ({ year, month, today, day, setYear, setMonth, setDay }) => {
           년
         </div>
         <div className="row__item">
-          <select name="month" defaultValue={month + 1}>
+          <select
+            name="month"
+            defaultValue={month + 1}
+            onChange={onMonthChange}
+          >
             {Array.from({ length: 12 }, (_, i) => i + 1).map(item => (
-              <option value={item} selected={month + 1 === item}>
+              <option value={item} selected={month + 1 === item} key={item}>
                 {item}
               </option>
             ))}
@@ -32,12 +55,14 @@ const DateInput = ({ year, month, today, day, setYear, setMonth, setDay }) => {
           월
         </div>
         <div className="row__item">
-          <select name="date" defaultValue={day}>
+          <select name="date" defaultValue={day} onChange={onDayChange}>
             {Array.from(
               { length: calculateDate.getDaysInMonth(year, month + 1) },
               (_, i) => i + 1,
             ).map(item => (
-              <option value={item}>{item}</option>
+              <option value={item} key={item}>
+                {item}
+              </option>
             ))}
           </select>
           일
