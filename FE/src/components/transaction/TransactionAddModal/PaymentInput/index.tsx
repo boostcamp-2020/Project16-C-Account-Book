9bf5c2699a515b903b010b83f04fa38a819456c0
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-12-06 21:44:15 +0900

    [feat] 결제수단 데이터를 가지고 그리도록 수정

--- FE/src/components/transaction/TransactionAddModal/PaymentInput/index.tsx
+++ FE/src/components/transaction/TransactionAddModal/PaymentInput/index.tsx
@@ -2,60 +2,30 @@ import React from 'react';
 
 import './index.scss';
 
-const PaymentInput = () => {
+const PaymentInput = ({ paymentPool, onPaymentChange }) => {
   return (
     <div className="item payment__input">
       <div className="indicator">결제수단</div>
       <div className="payment__card__container">
-        <label className="payment__card__item">
-          <input type="radio" name="payment" />
-          <div className="payment__card__view">
-            <div className="payment__card__title">a</div>
-            <div className="payment__card__description">description</div>
-          </div>
-        </label>
-        <label className="payment__card__item">
-          <input type="radio" name="payment" />
-          <div className="payment__card__view">
-            <div className="payment__card__title">a</div>
-            <div className="payment__card__description">description</div>
-          </div>
-        </label>
-        <label className="payment__card__item">
-          <input type="radio" name="payment" />
-          <div className="payment__card__view">
-            <div className="payment__card__title">a</div>
-            <div className="payment__card__description">description</div>
-          </div>
-        </label>
-        <label className="payment__card__item">
-          <input type="radio" name="payment" />
-          <div className="payment__card__view">
-            <div className="payment__card__title">a</div>
-            <div className="payment__card__description">description</div>
-          </div>
-        </label>
-        <label className="payment__card__item">
-          <input type="radio" name="payment" />
-          <div className="payment__card__view">
-            <div className="payment__card__title">a</div>
-            <div className="payment__card__description">description</div>
-          </div>
-        </label>
-        <label className="payment__card__item">
-          <input type="radio" name="payment" />
-          <div className="payment__card__view">
-            <div className="payment__card__title">a</div>
-            <div className="payment__card__description">description</div>
-          </div>
-        </label>
-        <label className="payment__card__item">
-          <input type="radio" name="payment" />
-          <div className="payment__card__view">
-            <div className="payment__card__title">a</div>
-            <div className="payment__card__description">description</div>
-          </div>
-        </label>
+        {paymentPool.length ? (
+          paymentPool.map(({ name, desc }) => (
+            <label className="payment__card__item">
+              <input
+                type="radio"
+                name="payment"
+                data-name={name}
+                data-description={desc}
+                onClick={onPaymentChange}
+              />
+              <div className="payment__card__view">
+                <div className="payment__card__title">{name}</div>
+                <div className="payment__card__description">{desc}</div>
+              </div>
+            </label>
+          ))
+        ) : (
+          <h3 className="no__payment">결제수단을 설정해주세요.</h3>
+        )}
       </div>
     </div>
   );
