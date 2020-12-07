23d2b00abf6b0cc58b50cd62f6bb5c2b8d8ad892
msmk530 <msmk530@naver.com>
2020-12-07 22:50:23 +0900

    [refactor] setting페이지에서 새로고침 시 데이터 없을 것을 수정

--- FE/src/components/SettingContent/CalendarSetting/index.tsx
+++ FE/src/components/SettingContent/CalendarSetting/index.tsx
@@ -3,5 +3,50 @@ import React from 'react';
 import './calendarSetting.scss';
 
 export default function CalendarSetting(props) {
-  return <div className="calndar__setting__container">calendar</div>;
+  return (
+    <div className="calendar__setting__container">
+      <div className="calendar__setting__startday">
+        <div className="calendar__setting__title">Start Day</div>
+        <div className="select__startday">
+          <label>
+            SUN
+            <input type="radio" id="sunday" name="startday" />
+          </label>
+
+          <label>
+            MON
+            <input type="radio" id="monday" name="startday" />
+          </label>
+
+          <label>
+            TUE
+            <input type="radio" id="tuesday" name="startday" />
+          </label>
+
+          <label>
+            WED
+            <input type="radio" id="wednesday" name="startday" />
+          </label>
+
+          <label>
+            THU
+            <input type="radio" id="thursday" name="startday" />
+          </label>
+
+          <label>
+            FRI
+            <input type="radio" id="friday" name="startday" />
+          </label>
+
+          <label>
+            SAT
+            <input type="radio" id="saturday" name="startday" />
+          </label>
+        </div>
+      </div>
+      <div className="calendar__setting__GMT">
+        <div className="calendar__setting__title">GMT</div>
+      </div>
+    </div>
+  );
 }
--- FE/src/pages/SettingPage/index.tsx
+++ FE/src/pages/SettingPage/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import SettingBar from '../../components/SettingBar';
 
@@ -6,14 +7,23 @@ import UserSetting from '../../components/SettingContent/UserSetting';
 import CalendarSetting from '../../components/SettingContent/CalendarSetting';
 import CategorySetting from '../../components/SettingContent/CategorySetting';
 import CSVSetting from '../../components/SettingContent/CSVSetting';
+import useAccountBook from '../../service/useAccountBookSetting';
 
 import './settingPage.scss';
 
 export default function SettingPage() {
-  const [settingType, setSettingType] = useState('user');
+  const [settingType, setSettingType] = useState('calendar');
+  const history = useHistory();
+  const accountBookId = history.location.state;
+
+  useAccountBook(accountBookId);
   return (
     <div className="setting__page__wrapper">
-      <SettingBar settingType={settingType} setSettingType={setSettingType} />
+      <SettingBar
+        accountBookId={accountBookId.id}
+        settingType={settingType}
+        setSettingType={setSettingType}
+      />
       {settingType === 'user' && <UserSetting />}
       {settingType === 'calendar' && <CalendarSetting />}
       {settingType === 'category' && <CategorySetting />}
