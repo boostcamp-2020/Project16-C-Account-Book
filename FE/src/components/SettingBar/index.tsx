b71de1cfda51f15e6f331259329f0cbb16706e69
msmk530 <msmk530@naver.com>
2020-12-07 12:23:18 +0900

    [feat] setting bar 설정

--- FE/src/components/SettingBar/index.tsx
+++ FE/src/components/SettingBar/index.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import './settingBar.scss';
 
 export default function SettingBar(props) {
-  return <div className="setting__bar__container">hello</div>;
+  const history = useHistory();
+  const id = history.location.state;
+
+  const onClickBackBtn = event => {
+    history.push({
+      pathname: event.target.dataset.type,
+      state: {
+        id,
+      },
+    });
+  };
+  return (
+    <header className="settingbar__header">
+      <div className="settingbar__buttons">
+        <div
+          data-type="/calendar"
+          className="settingbar__back__navBtn checked"
+          onClick={onClickBackBtn}
+        >
+          <i data-type="/calendar" className="fas fa-arrow-left" />
+          <span data-type="/calendar">Account Book</span>
+        </div>
+        <div className="settingbar__navBtn checked">
+          <i className="fas fa-users" />
+        </div>
+        <div className="settingbar__navBtn checked">
+          <i className="far fa-calendar-alt" />
+        </div>
+        <div className="settingbar__navBtn checked">
+          <i className="fas fa-boxes" />
+        </div>
+        <div className="settingbar__navBtn checked">
+          <i className="fas fa-file-csv" />
+        </div>
+      </div>
+    </header>
+  );
 }
