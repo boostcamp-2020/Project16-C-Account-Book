2b45a0fa3d971c066f07ad592abc4b63c8bd9190
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-12-08 00:01:51 +0900

    [feat] #44 가계부 모달 provider 추가

--- FE/src/app.tsx
+++ FE/src/app.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PaymentProvider from './store/PaymentMethod/paymentMethodContext';
 import DateInfoProvider from './store/DateInfo/dateInfoContext';
 import TransactionInfoProvider from './store/AccountBook/accountBookDataContext';
+import TransactionFormModalProvider from './store/TransactionFormModal/TransactionFormModalContext';
 
 import './app.scss';
 
@@ -30,7 +31,9 @@ const App = () => {
               <Route exact path="/calendar">
                 <CalendarPage />
               </Route>
-              <Route exact path="/transaction" component={TransactionPage} />
+              <TransactionFormModalProvider>
+                <Route exact path="/transaction" component={TransactionPage} />
+              </TransactionFormModalProvider>
               <Route exact path="/chart" component={ChartPage} />
             </Switch>
           </Router>
