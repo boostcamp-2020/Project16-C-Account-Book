3ace25d6be45fbcaa7fa366c85c747495e94e462
msmk530 <msmk530@naver.com>
2020-12-07 20:22:07 +0900

    [feat] 시작 요일에 따른 헤더 표기 조건부 렌더링

---
+++ FE/src/components/Calendar/CalendarHeader/index.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+
+import { useTransactionData } from '../../../store/AccountBook/accountBookInfoHook';
+
+export default function CalendarHeader(props) {
+  const startDay = useTransactionData(store => store.accountBook.startday);
+  return (
+    <>
+      {startDay === 'SUN' && (
+        <>
+          <th className="sunday">SUN</th>
+          <th>MON</th>
+          <th>TUE</th>
+          <th>WED</th>
+          <th>THU</th>
+          <th>FRI</th>
+          <th>SAT</th>
+        </>
+      )}
+      {startDay === 'MON' && (
+        <>
+          <th>MON</th>
+          <th>TUE</th>
+          <th>WED</th>
+          <th>THU</th>
+          <th>FRI</th>
+          <th>SAT</th>
+          <th className="sunday">SUN</th>
+        </>
+      )}
+      {startDay === 'TUE' && (
+        <>
+          <th>TUE</th>
+          <th>WED</th>
+          <th>THU</th>
+          <th>FRI</th>
+          <th>SAT</th>
+          <th className="sunday">SUN</th>
+          <th>MON</th>
+        </>
+      )}
+      {startDay === 'WED' && (
+        <>
+          <th>WED</th>
+          <th>THU</th>
+          <th>FRI</th>
+          <th>SAT</th>
+          <th className="sunday">SUN</th>
+          <th>MON</th>
+          <th>TUE</th>
+        </>
+      )}
+      {startDay === 'THU' && (
+        <>
+          <th>THU</th>
+          <th>FRI</th>
+          <th>SAT</th>
+          <th className="sunday">SUN</th>
+          <th>MON</th>
+          <th>TUE</th>
+          <th>WED</th>
+        </>
+      )}
+      {startDay === 'FRI' && (
+        <>
+          <th>FRI</th>
+          <th>SAT</th>
+          <th className="sunday">SUN</th>
+          <th>MON</th>
+          <th>TUE</th>
+          <th>WED</th>
+          <th>THU</th>
+        </>
+      )}
+      {startDay === 'SAT' && (
+        <>
+          <th>SAT</th>
+          <th className="sunday">SUN</th>
+          <th>MON</th>
+          <th>TUE</th>
+          <th>WED</th>
+          <th>THU</th>
+          <th>FRI</th>
+        </>
+      )}
+    </>
+  );
+}
