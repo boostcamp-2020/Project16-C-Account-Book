9223c96c0da8fcb7ed0d816d022d935795b616da
msmk530 <msmk530@naver.com>
2020-12-07 23:59:15 +0900

    [feat] start day를 설정하기 위한 틀 형성

--- FE/src/components/SettingContent/CalendarSetting/index.tsx
+++ FE/src/components/SettingContent/CalendarSetting/index.tsx
@@ -1,51 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import { useTransactionData } from '../../../store/AccountBook/accountBookInfoHook';
+
+import CalendarHeader from '../../Calendar/CalendarHeader';
 import './calendarSetting.scss';
 
 export default function CalendarSetting(props) {
+  const start = useTransactionData(store => store.accountBook.startday);
+
+  const [startDay, setStartDay] = useState('');
+  const weeks = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  useEffect(() => {
+    setStartDay(start);
+  }, [start]);
+
   return (
     <div className="calendar__setting__container">
       <div className="calendar__setting__startday">
         <div className="calendar__setting__title">Start Day</div>
         <div className="select__startday">
-          <label>
-            SUN
-            <input type="radio" id="sunday" name="startday" />
-          </label>
-
-          <label>
-            MON
-            <input type="radio" id="monday" name="startday" />
-          </label>
-
-          <label>
-            TUE
-            <input type="radio" id="tuesday" name="startday" />
-          </label>
-
-          <label>
-            WED
-            <input type="radio" id="wednesday" name="startday" />
-          </label>
-
-          <label>
-            THU
-            <input type="radio" id="thursday" name="startday" />
-          </label>
-
-          <label>
-            FRI
-            <input type="radio" id="friday" name="startday" />
-          </label>
-
-          <label>
-            SAT
-            <input type="radio" id="saturday" name="startday" />
-          </label>
+          {weeks.map(day => {
+            return (
+              <label>
+                {day}
+                <input
+                  type="radio"
+                  name="startday"
+                  value={day}
+                  checked={startDay === day}
+                  onChange={e => setStartDay(e.target.value)}
+                />
+              </label>
+            );
+          })}
         </div>
-      </div>
-      <div className="calendar__setting__GMT">
-        <div className="calendar__setting__title">GMT</div>
+        <div className="start__day__title">
+          <CalendarHeader startDay={startDay} />
+        </div>
+        <button className="calendar__setting__save">Save</button>
       </div>
     </div>
   );
