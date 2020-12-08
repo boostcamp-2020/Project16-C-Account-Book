a74b2a2295b5e6cf461fee46070ec369c686cd1f
msmk530 <msmk530@naver.com>
2020-12-08 14:12:29 +0900

    [feat] category api 작성

---
+++ FE/src/api/category.ts
@@ -0,0 +1,33 @@
+import { postFetch, updateFetch, deleteFetch } from '../service/fetch';
+
+export const createCategory = ({ accountBookId, name, type, icon }) => {
+  const data = postFetch(
+    `${process.env.SERVER_URL}/api/accountbook/${accountBookId}/category`,
+    {
+      name,
+      type,
+      icon,
+    },
+  );
+  return data;
+};
+
+export const updateCategory = ({ accountBookId, name, type, icon }) => {
+  const data = updateFetch(
+    `${process.env.SERVER_URL}/api/accountbook/${accountBookId}/category`,
+    {
+      name,
+      type,
+      icon,
+    },
+  );
+  return data;
+};
+
+export const deleteCategory = ({ accountBookId, categoryId }) => {
+  const data = deleteFetch(
+    `${process.env.SERVER_URL}/api/accountbook/${accountBookId}/category/${categoryId}`,
+    {},
+  );
+  return data;
+};
