c980ddd14478527b6bbd980e88510e7771ed67f9
msmk530 <msmk530@naver.com>
2020-12-08 09:27:23 +0900

    [refactor] event.stopPropagation을 통해 돔요소 내에서의 클릭이벤트 처리

--- FE/src/components/SettingContent/SaveModal/index.tsx
+++ FE/src/components/SettingContent/SaveModal/index.tsx
@@ -8,9 +8,8 @@ export default function SaveModal({ saveAction, updateData, setSaveModal }) {
   };
 
   const onClickOverlay = event => {
-    if (event.target.classList.contains('save__modal__overlay')) {
-      setSaveModal(false);
-    }
+    setSaveModal(false);
+    event.nativeEvent.stopPropagtion();
   };
 
   const onClickSaveModalCancel = () => {
@@ -19,7 +18,7 @@ export default function SaveModal({ saveAction, updateData, setSaveModal }) {
 
   return (
     <div className="save__modal__overlay" onClick={onClickOverlay}>
-      <div className="save__modal__content">
+      <div className="save__modal__content" onClick={e => e.stopPropagation()}>
         <div className="save__modal__title">변경사항을 저장 하시겠습니까?</div>
         <button className="save__ok__btn" onClick={onClickSaveModalOk}>
           Ok
