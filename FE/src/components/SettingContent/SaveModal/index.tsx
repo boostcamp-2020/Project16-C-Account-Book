fed7c97846edb6874a23e1042922a33e1024460e
msmk530 <msmk530@naver.com>
2020-12-09 00:10:04 +0900

    [refactor] save modal에서 공통 컴포넌트 사용하도록 수정

--- FE/src/components/SettingContent/SaveModal/index.tsx
+++ FE/src/components/SettingContent/SaveModal/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import ActionButton from '../../Common/ActionButton';
 import './saveModal.scss';
 
 export default function SaveModal({ saveAction, updateData, setSaveModal }) {
@@ -21,12 +23,20 @@ export default function SaveModal({ saveAction, updateData, setSaveModal }) {
     <div className="save__modal__overlay" onClick={onClickOverlay}>
       <div className="save__modal__content" onClick={e => e.stopPropagation()}>
         <div className="save__modal__title">변경사항을 저장 하시겠습니까?</div>
-        <button className="save__ok__btn" onClick={onClickSaveModalOk}>
-          Ok
-        </button>
-        <button className="save__cancel__btn" onClick={onClickSaveModalCancel}>
-          Cancel
-        </button>
+        <div className="save__ok__btn">
+          <ActionButton
+            type="general"
+            content="Ok"
+            action={onClickSaveModalOk}
+          />
+        </div>
+        <div className="save__cancel__btn">
+          <ActionButton
+            type="general"
+            content="Cancel"
+            action={onClickSaveModalCancel}
+          />
+        </div>
       </div>
     </div>
   );
