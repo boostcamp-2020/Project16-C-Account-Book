990a84a554aa04236bb7895c759f77a24c131244
msmk530 <msmk530@naver.com>
2020-12-10 14:05:51 +0900

    [refactor] setting tab에서 사용되는 container와 box mixin분리

--- FE/src/components/SettingContent/UserSetting/index.tsx
+++ FE/src/components/SettingContent/UserSetting/index.tsx
@@ -3,5 +3,9 @@ import React from 'react';
 import './userSetting.scss';
 
 export default function UserSetting(props) {
-  return <div className="user__setting__container">user</div>;
+  return (
+    <div className="user__setting__container">
+      <div className="user__list__box">hello</div>
+    </div>
+  );
 }
