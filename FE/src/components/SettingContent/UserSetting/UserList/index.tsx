1ef2b8151011e92b74548f05839ec8b134beb92a
msmk530 <msmk530@naver.com>
2020-12-10 15:40:57 +0900

    [feat] 회원탈퇴 기능 연동

--- FE/src/components/SettingContent/UserSetting/UserList/index.tsx
+++ FE/src/components/SettingContent/UserSetting/UserList/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useAccountBookData } from '../../../../store/AccountBook/accountBookInfoHook';
-
+import ActionButton from '../../../Common/ActionButton';
+import { dropAccountBook } from '../../../../api/social';
 import './userList.scss';
+import { useHistory } from 'react-router-dom';
 
 export default function UserList(props) {
+  const history = useHistory();
   const users = useAccountBookData(store => store.accountBook.users);
+  const accountBookId = useAccountBookData(store => store.accountBook._id);
+  const onClickDropBtn = async () => {
+    const res = await dropAccountBook({ accountBookId });
 
+    history.push('/');
+  };
   return (
     <div className="user__list__box">
       {users &&
@@ -15,6 +23,7 @@ export default function UserList(props) {
             <div>{user.name}</div>
           </div>
         ))}
+      <ActionButton content="탈퇴하기" type="large" action={onClickDropBtn} />
     </div>
   );
 }
