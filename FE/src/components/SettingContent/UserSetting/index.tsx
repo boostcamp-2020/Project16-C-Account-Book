0b0f8d4ff4bb7b3fac51ba78198f6b7384cd92ec
msmk530 <msmk530@naver.com>
2020-12-10 14:35:19 +0900

    [feat] setting page의 user tab 레이아웃

--- FE/src/components/SettingContent/UserSetting/index.tsx
+++ FE/src/components/SettingContent/UserSetting/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import UserList from './UserList';
+import InviteCode from './InviteCode';
 import './userSetting.scss';
 
 export default function UserSetting(props) {
   return (
     <div className="user__setting__container">
-      <div className="user__list__box">hello</div>
+      <UserList />
+      <InviteCode />
     </div>
   );
 }
