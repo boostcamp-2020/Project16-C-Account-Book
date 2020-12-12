2adfaee7ba95b170d2b24d335d2f8c9d7ee0bd6c
hi0826 <kkkttt555@naver.com>
2020-12-12 15:40:04 +0900

    [refactor] 필요없는 console, 모듈 제거
    - 리팩토링

--- FE/src/api/auth.ts
+++ FE/src/api/auth.ts
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { postFetch } from '../service/fetch';
 
 const login = async (code: string, social: string) => {
@@ -7,7 +5,6 @@ const login = async (code: string, social: string) => {
   const tokenGetUrl = `${serverUrl}/api/auth/login`;
   try {
     const token = await postFetch(tokenGetUrl, { code, social });
-    console.log(token);
     window.localStorage.setItem('accessToken', token.accessToken);
     window.localStorage.setItem('refreshToken', token.refreshToken);
     return token;
