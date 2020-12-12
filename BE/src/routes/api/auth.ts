59db948ae17e11c64f746ab9156ea525556b5930
hi0826 <kkkttt555@naver.com>
2020-12-12 15:36:57 +0900

    [feat] #113 refresh token 라우팅
    - 라우팅

--- BE/src/routes/api/auth.ts
+++ BE/src/routes/api/auth.ts
@@ -4,5 +4,6 @@ import controller from '@controllers/auth';
 const router = new Router();
 
 router.post('/login', controller.login);
+router.post('/refresh', controller.refresh);
 
 export default router;
