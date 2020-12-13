a85a6e81cbf7f000cf43b2454aa5e9977f624a7d
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-12-14 00:10:47 +0900

    [feat] 가격에 천원마다 쉼표가 찍히도록 수정
    - 가격이 천원마다 쉼표가 찍히도록 수정했다.
    - 돈 옆에 원화 표시가 되도록 기능을 추가했다.

--- FE/src/components/transaction/TransactionAddModal/PriceInput/index.tsx
+++ FE/src/components/transaction/TransactionAddModal/PriceInput/index.tsx
@@ -1,10 +1,13 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useRef } from 'react';
 
 import { useTransactionAddModalData } from '../../../../store/TransactionFormModal/TransactionFormModalHook';
+import CommaMaker from '../../../../util/commaForMoney';
 
 import './index.scss';
 
 const PriceInput = () => {
+  const inputLabel = useRef(null);
+
   const { input, setInput } = useTransactionAddModalData(store => ({
     input: store.input,
     setInput: store.setInput,
@@ -14,21 +17,26 @@ const PriceInput = () => {
     if (e.target) {
       const targetElement = e.target as HTMLInputElement;
       setInput({ ...input, cost: +targetElement.value });
+      inputLabel.current.textContent = `₩ ${CommaMaker(+targetElement.value)}`;
+      console.log(inputLabel.current);
     }
   };
   return (
-    <div className="item price__input">
+    <div className="item price__input__container">
       <div className="indicator">금액</div>
-      <label>
+      <div className="price__input">
+        <label ref={inputLabel} htmlFor="price__input">
+          ₩ 0
+        </label>
         <input
+          id="price__input"
           type="number"
           name="price"
           onChange={onPriceChange}
           min="1"
-          placeholder="0"
           value={!!input.cost && input.cost}
         />
-      </label>
+      </div>
     </div>
   );
 };
