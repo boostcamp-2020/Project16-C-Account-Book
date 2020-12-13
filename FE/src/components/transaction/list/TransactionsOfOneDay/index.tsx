b371b86ce797f14d695b2d2b36319525289c638a
j03y14 <46087226+j03y14@users.noreply.github.com>
2020-12-14 00:20:34 +0900

    [feat] 드래그 중에 해당 날짜 컨테이너에 표시 및 드랍시 수정 기능 추가
    - 드래그가 들어왔을 때 중에 해당 날짜 컨테이너에 표시가 되는 기능을 추가했다.
    - 드래그가 나갔을 시에 날짜 컨테이너에서 표시가 사라지는 기능을 추가했다.
    - 드랍시에 해당 날짜로 드래그 된 거래내역의 정보를 수정하는 기능을 추가했다.

--- FE/src/components/transaction/list/TransactionsOfOneDay/index.tsx
+++ FE/src/components/transaction/list/TransactionsOfOneDay/index.tsx
@@ -1,31 +1,108 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import './index.scss';
 
+import { useHistory } from 'react-router-dom';
 import TransactionItem from '../TransactionItem';
+import { useAccountBookData } from '../../../../store/AccountBook/accountBookInfoHook';
 import { iTransactionsOfOneDay } from '../../../../types/transaction';
+import { updateTransaction as updateTransactionApi } from '../../../../api/transaction';
+import cursorInElement from '../../../../util/calculate-cursor';
 
 const TransactionsOfOneDay = ({
   date,
   transactions,
+  draggedItem,
+  setDraggedItem,
 }: iTransactionsOfOneDay) => {
-  return (
-    <div className="transactions__oneday__day">
-      <div className="transactions__oneday__daybar">{date}일</div>
-      <div className="transactions__oneday__container">
-        {transactions.map(t => (
-          <TransactionItem
-            date={date}
-            category={t.category}
-            content={t.content}
-            payment={t.payment}
-            cost={t.cost}
-            type={t.type}
-            id={t._id}
-            key={t._id}
-          />
-        ))}
+  const [draggedIn, setDraggedIn] = useState(false);
+  const accountBookId = useHistory().location.state.id;
+
+  const { getTransactionById, updateTransactionInStore } = useAccountBookData(
+    store => ({
+      getTransactionById: store.getTransactionById,
+      updateTransactionInStore: store.updateTransaction,
+    }),
+  );
+
+  const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDraggedIn(() => true);
+  };
+
+  const onDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    const elementBox = e.currentTarget.getBoundingClientRect();
+    const cursor = { clientX: e.clientX, clientY: e.clientY };
+
+    if (!cursorInElement(cursor, elementBox)) {
+      setDraggedIn(() => false);
+    }
+  };
+
+  const updateTransaction = async movedTransaction => {
+    const { status } = await updateTransactionApi(
+      accountBookId,
+      movedTransaction,
+    );
+
+    if (status !== 200) return alert('이동에 실패했습니다.');
+
+    updateTransactionInStore(movedTransaction);
+  };
+
+  const onDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const movedTransactionId = e.dataTransfer.getData('transactionId');
+    const movedTransaction = getTransactionById(movedTransactionId);
+    setDraggedIn(false);
+    if (movedTransaction.date === date) return;
+    movedTransaction.date = date;
+    updateTransaction(movedTransaction);
+    console.log('on drop:', movedTransactionId);
+  };
+
+  return useMemo(
+    () => (
+      <div
+        className={`transactions__oneday__day${
+          draggedIn ? ' transactions__oneday__day__draggedIn' : ''
+        }`}
+        onDrop={onDrop}
+        onDragOver={onDragOver}
+        onDragLeave={onDragLeave}
+      >
+        <label className="transactions__oneday__daybar">{date}일</label>
+        <ul className="transactions__oneday__container">
+          {transactions.map(t => (
+            <TransactionItem
+              dragObject={false}
+              date={date}
+              category={t.category}
+              content={t.content}
+              payment={t.payment}
+              cost={t.cost}
+              type={t.type}
+              id={t._id}
+              key={t._id}
+              setDraggedItem={setDraggedItem}
+            />
+          ))}
+          {draggedIn && draggedItem.date !== date && (
+            <TransactionItem
+              dragObject
+              date={date}
+              category={draggedItem.category}
+              content={draggedItem.content}
+              payment={draggedItem.payment}
+              cost={draggedItem.cost}
+              type={draggedItem.type}
+              id={draggedItem._id}
+            />
+          )}
+        </ul>
       </div>
-    </div>
+    ),
+    [transactions, draggedIn],
   );
 };
 
