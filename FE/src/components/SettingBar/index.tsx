02ff3afd0a10dc446bba5e21d855566d705c02a9
msmk530 <msmk530@naver.com>
2020-12-14 21:53:54 +0900

    [feat] setting page light mode  틀 설정

--- FE/src/components/SettingBar/index.tsx
+++ FE/src/components/SettingBar/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useAccountBookData } from '../../store/AccountBook/accountBookInfoHook';
 
-import HeaderButton from '../Common/HeaderButton';
 import SettingButtons from './SettingButtons';
+import ThemeButton from '../Common/ThemeButton';
+import { useThemeData } from '../../store/Theme/themeHook';
 import './settingBar.scss';
 
 export default function SettingBar({ settingType, setSettingType }) {
   const history = useHistory();
+  const theme = useThemeData(store => store.mode);
   const accountBookId = useAccountBookData(store => store.accountBook._id);
   const onClickBackBtn = event => {
     history.push({
@@ -19,22 +21,40 @@ export default function SettingBar({ settingType, setSettingType }) {
   };
 
   return (
-    <header className="settingbar__header">
+    <header
+      className={
+        theme === 'dark' ? 'settingbar__header' : 'settingbar__header light'
+      }
+    >
       <div className="settingbar__buttons">
         <div
           data-type="/calendar"
           className="settingbar__back__navBtn checked"
           onClick={onClickBackBtn}
         >
-          <i data-type="/calendar" className="fas fa-arrow-left" />
-          <span className="nav__accountbook__btn" data-type="/calendar">
+          <i
+            data-type="/calendar"
+            className={
+              theme === 'dark' ? 'fas fa-arrow-left' : 'fas fa-arrow-left light'
+            }
+          />
+          <span
+            className={
+              theme === 'dark'
+                ? 'nav__accountbook__btn'
+                : 'nav__accountbook__btn light'
+            }
+            data-type="/calendar"
+          >
             Account Book
           </span>
         </div>
+
         <SettingButtons
           settingType={settingType}
           setSettingType={setSettingType}
         />
+        <ThemeButton />
       </div>
     </header>
   );
