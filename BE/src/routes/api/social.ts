0d027700761714fea9871a64eab4bc2f3e99a26d
hi0826 <kkkttt555@naver.com>
2020-12-15 15:42:12 +0900

    [refactor] #142 social router 리팩토링
    - controller 절대경로로 변경
    - http 함수별로 구분

--- BE/src/routes/api/social.ts
+++ BE/src/routes/api/social.ts
@@ -1,11 +1,12 @@
 import Router from 'koa-router';
 
-import controller from '@controllers/social';
+import controller from '@/controllers/social';
 
 const router = new Router();
 
 // api/social
 
 router.post('/user', controller.post);
+
 router.delete('/:accountbookid/user', controller.del);
 export default router;
