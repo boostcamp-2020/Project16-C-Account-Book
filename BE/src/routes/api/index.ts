84ce6126dbb3dc21f816e196785a17c8ef500dea
hi0826 <kkkttt555@naver.com>
2020-12-15 15:40:16 +0900

    [refactor] #142 api router 리팩토링
    - 모듈 절대경로로 수정
    - 필요없는 모듈 삭제
    - 필요없는 코드 삭제
    - URI 알 수 있게 주석 추가
    - HTTP 함수 기능별로 정리

--- BE/src/routes/api/index.ts
+++ BE/src/routes/api/index.ts
@@ -1,23 +1,17 @@
-import { Context } from 'koa';
 import Router from 'koa-router';
 
-import authController from '@controllers/auth';
-import authRouter from './auth';
-import accountBookRouter from './account-book';
-import defaultPaymentMethodRouter from './defaultPaymentMethod';
-import socialRouter from './social';
+import authController from '@/controllers/auth';
+import authRouter from '@/routes/api/auth';
+import accountBookRouter from '@/routes/api/account-book';
+import defaultPaymentMethodRouter from '@/routes/api/defaultPaymentMethod';
+import socialRouter from '@/routes/api/social';
 
 const router = new Router();
 
-router.get('/', (ctx: Context, next) => {
-  ctx.body = `GET ${ctx.path}`;
-  next();
-});
+// api/
 
 router.use('/auth', authRouter.routes());
-
 router.use(authController.checkToken);
-
 router.use('/accountbook', accountBookRouter.routes());
 router.use('/social/accountbook', socialRouter.routes());
 router.use('/defaultPaymentMethod', defaultPaymentMethodRouter.routes());
