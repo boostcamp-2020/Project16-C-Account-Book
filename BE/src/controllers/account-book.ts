25e641a547f27c54fba4b0b7d7569c6a5470b8ff
hi0826 <kkkttt555@naver.com>
2020-12-15 18:07:33 +0900

    [refactor] #142 accountbook 리팩토링
    - service로 ctx를 바로 넘기지 않게 처리
    - 절대경로 수정

--- BE/src/controllers/account-book.ts
+++ BE/src/controllers/account-book.ts
@@ -1,64 +1,73 @@
 import { Context } from 'koa';
-import service from '@services/account-book';
-import response from '@utils/response';
+
+import service from '@/services/account-book';
+
+import response from '@/utils/response';
 
 const get = async (ctx: Context): Promise<Context['body']> => {
-  const accountBooks = await service.get(ctx);
-  const res = response(200, '', accountBooks);
+  const userInfo = ctx.user;
+  const accountBooks = await service.get(userInfo);
+  const res = response(200, { accountBooks });
   ctx.status = res.status;
-  ctx.body = res;
-
-  return ctx.body;
-};
-
-const post = async (ctx: Context): Promise<Context['body']> => {
-  const accountBook = await service.post(ctx);
-  const res = response(200, '', accountBook);
-  ctx.status = res.status;
-  ctx.body = res;
-
-  return ctx.body;
-};
-
-const update = async (ctx: Context): Promise<Context['body']> => {
-  const updateResult = await service.patch(ctx);
-  const res = response(200, '', updateResult.data);
-  ctx.status = res.status;
-  ctx.body = res;
-  return ctx.body;
-};
-
-const updateStartday = async (ctx: Context): Promise<Context['body']> => {
-  const updateResult = await service.patchStartday(ctx);
-  const res = response(200, '', updateResult.data);
-  ctx.status = res.status;
-  ctx.body = res;
-  return ctx.body;
-};
-
-const del = async (ctx: Context): Promise<Context['body']> => {
-  const deleteResult = await service.del(ctx);
-  const res = response(200, '', deleteResult);
-  ctx.status = res.status;
-  ctx.body = res;
-
+  ctx.body = res.data;
   return ctx.body;
 };
 
 const getDetail = async (ctx: Context): Promise<Context['body']> => {
-  const accountBook = await service.getDetail(ctx);
-  const res = response(200, '', accountBook);
+  const { params } = ctx;
+  const accountBook = await service.getDetail(params);
+  const res = response(200, { accountBook });
   ctx.status = res.status;
-  ctx.body = res;
+  ctx.body = res.data;
   return ctx.body;
 };
 
 const getCode = async (ctx: Context): Promise<Context['body']> => {
-  const accountData = await service.getCode(ctx);
-  const res = response(200, '', accountData);
+  const { params } = ctx;
+  const code = await service.getCode(params);
+  const res = response(200, { code });
   ctx.status = res.status;
-  ctx.body = res;
+  ctx.body = res.data;
   return ctx.body;
 };
 
-export default { get, post, update, updateStartday, del, getDetail, getCode };
+const post = async (ctx: Context): Promise<Context['body']> => {
+  const userInfo = ctx.user;
+  const { body } = ctx.request.body;
+  const accountBook = await service.post(userInfo, body);
+  const res = response(200, { accountBook });
+  ctx.status = res.status;
+  ctx.body = res.data;
+  return ctx.body;
+};
+
+const update = async (ctx: Context): Promise<Context['body']> => {
+  const { params } = ctx.params;
+  const { body } = ctx.request.body;
+  const accountBookInfo = await service.patch(params, body);
+  const res = response(200, { accountBookInfo });
+  ctx.status = res.status;
+  ctx.body = res.data;
+  return ctx.body;
+};
+
+const updateStartday = async (ctx: Context): Promise<Context['body']> => {
+  const { params } = ctx.params;
+  const { body } = ctx.request.body;
+  const accountBookInfo = await service.patchStartday(params, body);
+  const res = response(200, { accountBookInfo });
+  ctx.status = res.status;
+  ctx.body = res.data;
+  return ctx.body;
+};
+
+const del = async (ctx: Context): Promise<Context['body']> => {
+  const { params } = ctx.params;
+  const deleteResult = await service.del(params);
+  const res = response(200, { deleteResult });
+  ctx.status = res.status;
+  ctx.body = res.data;
+  return ctx.body;
+};
+
+export default { get, getDetail, getCode, post, update, updateStartday, del };
