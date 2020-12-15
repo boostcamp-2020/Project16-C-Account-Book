71232b0440e160e917037a5e0db408a03fad09cc
hi0826 <kkkttt555@naver.com>
2020-12-15 15:36:30 +0900

    [refactor] #142 진입점 리팩토링
    - config 모듈 추가로 config 관련부분 리팩토링
    - listen 결과를 콜백으로 변경

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,17 +1,15 @@
 import 'module-alias/register';
 import 'reflect-metadata';
 import Koa from 'koa';
-import dotenv from 'dotenv';
-import path from 'path';
-import loaders from './loaders';
-
-dotenv.config({ path: path.join(__dirname, '../.env') });
+import config from '@/config';
+import loaders from '@/loaders';
 
 const startServer = async (): Promise<void> => {
   const app = new Koa();
   await loaders(app);
-  app.listen(process.env.PORT);
-  console.info(`http://localhost:${process.env.PORT} ✅`);
+  app.listen(config.port, () => {
+    console.info(`http://localhost:${config.port} ✅`);
+  });
 };
 
 startServer();
