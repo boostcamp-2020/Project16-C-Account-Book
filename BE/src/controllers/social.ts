93a9ca956800173ba0a049be6341acaabad08092
hi0826 <kkkttt555@naver.com>
2020-12-15 17:49:47 +0900

    [refactor] #142 social 컨트롤러 리팩토링
    - service 넘겨줄 때 ctx 직접 넘기지 않도록 변경
    - response 만들어서 return 해주게 변경

--- BE/src/controllers/social.ts
+++ BE/src/controllers/social.ts
@@ -1,18 +1,26 @@
 import { Context } from 'koa';
-import service from '@services/account-book';
-import response from '@utils/response';
+
+import service from '@/services/account-book';
+
+import response from '@/utils/response';
 
 const post = async (ctx: Context): Promise<Context['body']> => {
-  const result = await service.addUser(ctx);
-  const res = response(200, '', result);
-  ctx.body = res;
+  const userInfo = ctx.user;
+  const codeInfo = ctx.request.body;
+  const accountBook = await service.addUser(codeInfo, userInfo);
+  const res = response(200, { accountBook });
+  ctx.status = res.status;
+  ctx.body = res.data;
   return ctx.body;
 };
 
 const del = async (ctx: Context): Promise<Context['body']> => {
-  const result = await service.delUser(ctx);
-  const res = response(200, '', result);
-  ctx.body = res;
+  const userInfo = ctx.user;
+  const paramInfo = ctx.params;
+  const result = await service.delUser(paramInfo, userInfo);
+  const res = response(200, { result });
+  ctx.status = res.status;
+  ctx.body = res.data;
   return ctx.body;
 };
 
