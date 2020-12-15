b18ad8c88a0fc119da10feef8a86340fe13128f3
hi0826 <kkkttt555@naver.com>
2020-12-15 19:24:34 +0900

    [refactor] #142 error 핸들러 리팩토링
    - response 로 error message 보내주기
    - 에러 로그 띄우기

--- BE/src/loaders/error.ts
+++ BE/src/loaders/error.ts
@@ -3,10 +3,7 @@ import Koa from 'koa';
 import ResponseError from '@/interfaces/error';
 import response from '@/utils/response';
 
-const errorCatcher = async (
-  ctx: Koa.Context,
-  next: () => Promise<void>,
-): Promise<void> => {
+const errorCatcher = async (ctx: Koa.Context, next: () => Promise<void>): Promise<void> => {
   try {
     await next();
   } catch (err) {
@@ -16,7 +13,7 @@ const errorCatcher = async (
 
 const errorHandler = (err: ResponseError, ctx: Koa.Context): void => {
   const status = err.status || 500;
-  const res = response(status, '', {});
+  const res = response(status, { message: err.message });
   ctx.status = res.status;
   ctx.body = res;
   console.error(`error code: [${ctx.status}], message: ${err.message}`);
