48158a62506e1dc979fa32f94d706ed645c840f1
hi0826 <kkkttt555@naver.com>
2020-12-15 15:37:06 +0900

    [refactor] #142 loaders 진입점 리팩토링
    - 모듈 절대경로로 변경

--- BE/src/loaders/index.ts
+++ BE/src/loaders/index.ts
@@ -1,8 +1,8 @@
 import Koa from 'koa';
 
-import mongooseLoader from './mongoose';
-import koaLoader from './koa';
-import { redisLoader } from './redis';
+import mongooseLoader from '@/loaders/mongoose';
+import koaLoader from '@/loaders/koa';
+import { redisLoader } from '@/loaders/redis';
 
 export default async (
   koaApp: Koa<Koa.DefaultState, Koa.DefaultContext>,
