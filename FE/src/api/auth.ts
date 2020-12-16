521fd2d534d05cf61e89c61d015094c5ea00016a
msmk530 <msmk530@naver.com>
2020-12-16 21:46:42 +0900

    [fix] token 리스폰스 포맷에 맞도록 수정

--- FE/src/api/auth.ts
+++ FE/src/api/auth.ts
@@ -5,8 +5,8 @@ const login = async (code: string, social: string): Promise<void> => {
   const tokenGetUrl = `${serverUrl}/api/auth/login`;
   try {
     const token = await postFetch(tokenGetUrl, { code, social });
-    window.localStorage.setItem('accessToken', token.accessToken);
-    window.localStorage.setItem('refreshToken', token.refreshToken);
+    window.localStorage.setItem('accessToken', token.data.accessToken);
+    window.localStorage.setItem('refreshToken', token.data.refreshToken);
   } catch (error) {
     console.error(error);
   }
