af9712852a57f5d95443bdd4d84a68b2e06ec293
hi0826 <kkkttt555@naver.com>
2020-12-17 12:04:19 +0900

    [feat] #113 리프레시토큰 수정
    - try catch 삭제

--- FE/src/api/refresh.ts
+++ FE/src/api/refresh.ts
@@ -3,19 +3,17 @@ import { postFetch } from '../service/fetch';
 const refresh = async () => {
   const serverUrl = process.env.SERVER_URL;
   const tokenGetUrl = `${serverUrl}/api/auth/refresh`;
+  window.localStorage.removeItem('accessToken');
   const refreshToken = window.localStorage.getItem('refreshToken');
-  try {
-    const response = await postFetch(tokenGetUrl, { refreshToken });
-    window.localStorage.setItem('accessToken', response.newAccessToken);
-    if (response.status === 401) {
-      window.localStorage.removeItem('accessToken');
-      window.localStorage.removeItem('refreshToken');
-      return false;
-    }
-    return true;
-  } catch (err) {
-    console.error(err);
+
+  const response = await postFetch(tokenGetUrl, { refreshToken });
+  window.localStorage.setItem('accessToken', response.newAccessToken);
+  if (response.status === 401) {
+    window.localStorage.removeItem('accessToken');
+    window.localStorage.removeItem('refreshToken');
+    return false;
   }
+  return true;
 };
 
 export default refresh;
